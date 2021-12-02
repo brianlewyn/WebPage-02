f78d41db695f9839dc337027db13af49acf92346
DeveloperBrian <BrianMejMor@gmail.com>
2021-12-02 01:55:00 -0600

    Tan solo falta, la conclusión y el apartado somos

--- JS/Main.js
+++ JS/Main.js
@@ -22,3 +22,46 @@ function scrollActive(){
    })
 }
 window.addEventListener('scroll', scrollActive)
+
+
+/* ========== Scroll Reveal Animation ========== */
+const sr = ScrollReveal({
+   origin: 'top',
+   distance: '4px',
+   duration: 2500,
+   delay: 200
+  //  reset: true
+})
+sr.reveal(`
+#Introduction,
+  #WhatAre-A,
+  #IsTheSame-A,
+  #HowDoesItWork-A,
+  #HowToCreate-A,
+
+     #Habit01-A,
+     #Habit02-A,
+     #Habit03-A,
+     #Habit04-A,
+     #Habit05-A,
+     #Habit06-A,
+     #Habit07-A,
+     #Habit08-A,
+     #Habit09-A,
+     #Habit10-A,
+
+#Conclusion,
+
+#Apps,
+#Books,
+#More
+`)
+
+
+/* ========== Btn Music Animation ========== */
+let Btn = document.getElementById('Btn-Music')
+let Aside = document.querySelector('aside')
+
+Btn.onclick = function() {
+	Aside.classList.toggle('Box-Music')
+}
